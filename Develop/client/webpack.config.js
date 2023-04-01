3fa69536ba81e76bd081e0c3904ab4adbcac251b
Chrisbchicken <chrsbnjmn@gmail.com>
2023-04-01 15:45:29 -0500

    changed .config to service-worker.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -25,22 +25,22 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'JATE'
       }),
      
-      // Injects our custom service worker
+      //Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
+        swDest: 'service-worker.js',
       }),
 
       // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just a text editor',
+        short_name: 'JATE',
+        description: 'Never forget tasks!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
